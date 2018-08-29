32c02200bac1dd3ae4ae8befc8bbc10b8350b568
yanxi <testerlyx@foxmail.com>
2018-08-29 14:53:32 +0800

    add docker build image demo

---
+++ README.md
@@ -0,0 +1,7 @@
+## Demo for custom make docker image
+
+#### ref: https://docs.docker.com/compose/gettingstarted/#prerequisites
+
+## Environment:
+
+#### Python3.6 + redis + flask
---
+++ app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+"""
+https://docs.docker.com/compose/gettingstarted/#prerequisites
+need to change redis host="redis"
+"""
+
+import time
+import redis
+
+from flask import Flask
+
+app = Flask(__name__)
+cache = redis.Redis(host='127.0.0.1', port=6379)
+
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
+
+
+@app.route('/')
+def hello():
+    count = get_hit_count()
+    return 'Hello World! I have been seen {} times.\n'.format(count)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", debug=True)
